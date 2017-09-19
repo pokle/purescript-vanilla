d0d5c3488af9ea23be283adda357526df31d8b74
Tushar Pokle <tushar.pokle@gmail.com>
2017-09-19 21:39:43 +1000

    Coming to terms with the fact that there's no Any type in purescript

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,13 @@
+
 exports.clog = function(s) {
   return function() {
-    console.log('|--| ', s);
+    console.log('|-<>-| ', s);
     return {};
   };
 };
+
+exports.getElementById = function(s) {
+    return function() {
+        return document.getElementById(s);
+    }
+}
--- src/Main.purs
+++ src/Main.purs
@@ -5,14 +5,17 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 
 
--- foreign import clog :: String -> Eff (console :: CONSOLE | e)
 
--- | Write a message to the console.
-foreign import clog
-  :: forall eff
-   . String
-  -> Eff (console :: CONSOLE | eff) Unit
+foreign import clog :: forall eff . Cloggable -> Eff (console :: CONSOLE | eff) Unit
+
+foreign import data DOMElement :: Type
+
+data Cloggable = CloggableString String | CloggableDOMElement DOMElement
+
+foreign import getElementById :: forall eff . String -> Eff (eff) DOMElement
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  clog "Hello Space sailor"
+  clog (CloggableString "Hello Space sailor")
+  elem <- getElementById "app"
+  clog (CloggableDOMElement elem)
